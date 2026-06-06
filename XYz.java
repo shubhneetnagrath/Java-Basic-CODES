bbe6fffc2d2afcc9da4eab8103687ebda37a04b4
Shubhneet Nagrath <shubhneetnagrath@gmail.com>
2026-06-06 19:42:23 +0530

    Add initial implementation of light status checker in XYz.java

---
+++ XYz.java
@@ -0,0 +1,25 @@
+
+public class XYz {
+   public static void main(String[] args) {
+    
+    var light = 2 ;
+    
+    if (light==1){
+    System.out.println("light is on");
+    }
+    else if(light==2){
+    
+   System.out.println("light is bright");
+
+    }      
+
+   else{
+      System.out.println(" light is off");
+   }
+
+
+   } 
+
+
+
+}
