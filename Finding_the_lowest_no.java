d8a7c77b674a9f447b99fa6658ad175b67b1f082
Shubhneet Nagrath <shubhneetnagrath@gmail.com>
2026-06-10 15:35:46 +0530

    Array Practice

--- Finding_the_lowest_no.java
+++ Finding_the_lowest_no.java
@@ -2,8 +2,6 @@ public class Finding_the_lowest_no {
 public static void main(String[] args) {
    // An array storing different ages
    int ages[]={20,22,18,35,48,26,87,70};
-   // Get the lenght of the Array
-   int length = ages.length;
    // Create a 'lowest age' variable and assign the first array element of ages to it 
    int lowestAge =ages[0];
    // Loop through the elements of the ages array to find the lowest age
